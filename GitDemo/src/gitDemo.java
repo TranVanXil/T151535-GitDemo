23c2479a3417d609826c971487cd2a320832b8d7
Tran Van Xil <tranvanxil69@gmail.com>
2016-11-02 17:08:39 +0700

    init project

---
+++ GitDemo/src/gitDemo.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+
+public class gitDemo extends JFrame {
+
+	public static void main(String[] args){
+		gitDemo mainWindow = new gitDemo();
+		mainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mainWindow.setVisible(true);
+		
+	}
+}
